35311c4fe53320e2bcd424d6b9f0425e156de1c0
bhaaryan <133786477+bhaaryan@users.noreply.github.com>
2023-05-23 09:47:20 -0400

    Update Student.java
    comment added at 9:48

--- src/studentlist/Student.java
+++ src/studentlist/Student.java
@@ -9,6 +9,7 @@ public class Student {
     private String sname;
     private String sID;
 // this has been edited on github.com to show fetchmerge
+// some comment at 9:48
     public Student(String sname, String sID) {
         this.sname = sname;
         this.sID = sID;
